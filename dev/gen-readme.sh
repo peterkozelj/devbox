cc2c26d89b125e5551c58c2f21e72d55d92c084c
Peter Koželj <>
2020-04-23 12:12:32 +0200

    Fix readme generation

--- dev/gen-readme.sh
+++ dev/gen-readme.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cargo readme -r devbox -t ../dev/README.tpl -o ../README
-cargo readme -r devbox -t ../dev/README.tpl -o README
-cargo readme -r devbox-test -t ../dev/README.tpl -o README
-cargo readme -r devbox-build -t ../dev/README.tpl -o README
+cargo readme -r devbox -t ../dev/README.tpl -o ../README.md
+cargo readme -r devbox -t ../dev/README.tpl -o README.md
+cargo readme -r devbox-test -t ../dev/README.tpl -o README.md
+cargo readme -r devbox-build -t ../dev/README.tpl -o README.md
